309fa1939bec8b0268caac627bba3450dbb2c113
Adam Bernard <ab255@me.com>
2016-08-24 17:53:07 -0600

    timer count down

--- main.bundle.js
+++ main.bundle.js
@@ -39,9 +39,3 @@
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
-
-	
