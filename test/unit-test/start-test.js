b8eb5066de7f186ee149bc3e0c5639a5186f78ac
Mike Sausa <mikejsausa@gmail.com>
2016-08-29 11:27:41 -0600

    added start tests

---
+++ test/unit-test/start-test.js
@@ -0,0 +1,12 @@
+require('./lib/scripts/index');
+const assert = require('chai').assert;
+const $ = require('jquery');
+
+describe('start', function(){
+  const timer = new Timer(25, 0, 25);
+
+  it('should decrement the time', function(){
+    tick();
+    assert.lessThan(timer.remainingTime, 25);
+  });
+});
