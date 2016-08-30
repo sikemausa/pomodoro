67fa5921c6454a0d4765dd40c4e474b26895a229
Adam Bernard <ab255@me.com>
2016-08-30 16:29:26 -0600

    refactor

--- test/unit-test/start-test.js
+++ test/unit-test/start-test.js
@@ -3,8 +3,17 @@ const assert = require('chai').assert;
 const $ = require('jquery');
 
 describe('start', function(){
-  const timer = new Timer(25, 0, 25);
+  beforeEach(function(){
+    const timer = new Timer(25, 0, 25);
 
+  });
+  afterEach(function() {
+
+  });
+
+  it('should change css atributes', function() {
+    
+  })
   it('should decrement the time', function(){
     tick();
     assert.lessThan(timer.remainingTime, 25);
