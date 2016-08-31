a3a1b3b44f3017988b08787f5921abc4e341758d
Mike Sausa <mikejsausa@gmail.com>
2016-08-31 10:42:27 -0600

    fixed chromedriver

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
     loaders: [
       { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
       { test: /\.css$/, loader: "style!css" },
-      { test: /\.scss$/, loader: "style!css!sass" }
+      { test: /\.scss$/, loader: "style!css!sass" },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015' }
     ]
   },
   resolve: {
